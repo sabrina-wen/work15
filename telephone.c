0fcabba38979a1c832b9754e6eb08ed82781c776
sabrina-wen <sabrinawn253@gmail.com>
2017-12-08 21:37:53 -0500

    -v works i think and i added a header file

--- telephone.c
+++ telephone.c
@@ -1,20 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/sem.h>
-#include <string.h>
-#include <errno.h>
-#include <sys/shm.h>
-#include <sys/types.h>
-#include <fcntl.h>
-#define SEMKEY 46
-#define MEMKEY 121
+#include "headers.h"
 
 int main(int argc, char * argv[]) {
   if (argc == 2 && strcmp(argv[1], "-c") == 0) {
     //printf("hi\n");
-    
+
     // creates semaphore, note the permissions
     int sem_id = semget(SEMKEY, 1, IPC_CREAT | IPC_EXCL | 0777);
     if (sem_id == -1) {
@@ -33,20 +22,38 @@ int main(int argc, char * argv[]) {
       printf("mem id: %d\n", mem_id);
 
       // create file
-      int fd = open("story.txt", O_CREAT | O_RDWR | O_APPEND, 0644);
+      int fd = open("story.txt", O_CREAT | O_RDWR | O_TRUNC, 0777);
     }
-  } else if (argc == 2 && strcmp(argv[1], "-v") == 0) {
-    int sem_id = semget(SEMKEY, 0, 0777);
-    if ((semctl(sem_id, 0, GETVAL)) == -1) {
-      printf("Semctl error: %s\n", strerror(errno));
-    } else {
-      printf("value set: %d\n", semctl(sem_id, 0, GETVAL));
-    }
-  } else if (argc == 2 && strcmp(argv[1], "-r") == 0) {
+  }
+
+  else if (argc == 2 && strcmp(argv[1], "-v") == 0) {
+    char story[1000];
+    int fd = open("story.txt", O_RDWR);
+    read(fd, &story, sizeof(char) * 1000);
+    printf("story contents:\n%s",story);
+    close(fd);
+  }
+
+  else if (argc == 2 && strcmp(argv[1], "-r") == 0) {
+    // removing semaphore
     int sem_id = semget(SEMKEY, 0, 0777);
     semctl(sem_id, 0, IPC_RMID);
     printf("semaphore removed: %d\n", sem_id);
-  } else {
+
+    // removing mem
+    int mem_id = shmget(MEMKEY, sizeof(int), IPC_EXCL);
+    shmctl(mem_id, IPC_RMID, NULL);
+    printf("shared mem removed: %d\n", mem_id);
+
+    //does removing the story just meaning erasing its contents?
+    // need to wait until semaphore reaches 0
+    char story[1000];
+    int fd = open("story.txt", O_RDWR);
+    read(fd, &story, sizeof(char) * 1000);
+    printf("story contents:\n%s\n",story);
+  }
+
+  else {
     printf("args invalid\n");
   }
   return 0;
